98421440f87958fe2f3d0e2ee8d6cc744b53f080
Femi Ayodeji <femidotayodeji@gmail.com>
2021-03-14 11:37:30 +0100

    Add Paddle

--- src/app.js
+++ src/app.js
@@ -1,4 +1,12 @@
+import Paddle from '/src/paddle';
+
 let canvas = document.getElementById("game-screen");
 let context = canvas.getContext("2d");
 
-context.clearRect(0, 0, 800, 600);
+const GAME_WIDTH = 800;
+const GAME_HEIGHT = 600;
+
+context.clearRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
+
+let paddle = new Paddle(GAME_WIDTH, GAME_HEIGHT);
+paddle.draw(context);
