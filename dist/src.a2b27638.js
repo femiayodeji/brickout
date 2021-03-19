a1dd4f16a35895bd5d7d0eef458cbb651e0921e6
Femi Ayodeji <femidotayodeji@gmail.com>
2021-03-19 15:48:56 +0100

    Add heart image

--- dist/src.a2b27638.js
+++ dist/src.a2b27638.js
@@ -410,11 +410,7 @@ function buildLevel(game, level) {
   return bricks;
 }
 
-var level1 = [// [1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0, 0, 1, 1],
-// [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-// [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-// [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]];
+var level1 = [[1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0, 0, 1, 1], [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]];
 exports.level1 = level1;
 var level2 = [[1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0, 0, 1, 1]];
 exports.level2 = level2;
@@ -634,7 +630,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "1803" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "11817" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
